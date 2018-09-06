332af64d4424aab0d575e94fb187461f639b8ae9
cyj0619 <cyj0619@midasit.com>
2018-09-07 08:54:20 +0900

    Repository 인터페이스 생성

--- src/main/java/com/midasit/bungae/board/repository/BoardMemoryRepository.java
+++ src/main/java/com/midasit/bungae/board/repository/BoardMemoryRepository.java
@@ -6,7 +6,7 @@ import com.midasit.bungae.board.dto.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoardMemoryRepository {
+public class BoardMemoryRepository implements Repository {
     List<Board> boardList = null;
 
     public BoardMemoryRepository() {
---
+++ src/main/java/com/midasit/bungae/board/repository/Repository.java
@@ -0,0 +1,7 @@
+package com.midasit.bungae.board.repository;
+
+public interface Repository {
+    // 서비스에 필요한 메소드 정의 -> 구현 클래스 사용해서 바로 사용할 수 있도록
+    // CURD
+    // 기타 데이터 호출
+}
